448be454e49544ef9b577b4586f87dcef96c81fd
Francis Carino <fgcarino@gmail.com>
2022-08-02 16:12:15 -0700

    added notes per review feedback

--- queries.sql
+++ queries.sql
@@ -1,10 +1,10 @@
 -- write your queries here
-SELECT owners.*, vehicles.* FROM owners
+SELECT * FROM owners
     FULL OUTER JOIN vehicles
     ON vehicles.owner_id = owners.id;
 
 
-SELECT owners.first_name, count(vehicles.*)
+SELECT owners.first_name, count(*)
     FROM owners
     JOIN vehicles
     ON owners.id = vehicles.owner_id
@@ -15,6 +15,6 @@ SELECT owners.first_name, count(vehicles.*)
 SELECT o.first_name, ROUND(AVG(v.price),0) AS avg_price, count(*) as count
     FROM owners AS o
     JOIN vehicles AS v ON o.id = v.owner_id
-    GROUP BY o.first_name
+    GROUP BY o.id
     HAVING AVG(v.price) > 10000 AND count(*) > 1
     ORDER BY o.first_name DESC;
